63444175b623135b30c75aec27319620f5e559a0
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:38:04 -0500

    Complete 'Leap Years' Exercise

--- foundations/javascript-exercises/06_leapYears/leapYears.js
+++ foundations/javascript-exercises/06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (divisibleBy4(year) && (!divisibleBy100(year) || divisibleBy400(year))) {
+        return true
+    } else {
+        return false
+    }
 };
+
+function divisibleBy4(year) {
+    return (year % 4 === 0);
+}
+
+function divisibleBy100(year) {
+    return (year % 100 === 0);
+}
+
+function divisibleBy400(year) {
+    return (year % 400 === 0);
+}
 
 // Do not edit below this line
 module.exports = leapYears;
