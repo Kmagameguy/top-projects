aa3cdba61dc5b2e6cd8f03ffe1d3c4fcd6017827
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:38:58 -0500

    Complete 'Fibonacci' Exercise

--- foundations/javascript-exercises/10_fibonacci/fibonacci.js
+++ foundations/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) return 'OOPS';
+    let start = 0;
+    let end   = 1;
 
+    for (let i = 1; i < index; i++) {
+        const prevValue = end;
+        end = start + prevValue;
+        start = prevValue;
+    }
+
+    return end;
 };
 
 // Do not edit below this line
