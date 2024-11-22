153e24c3a66691f003a637b76c2e190cec2d77dd
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:43:56 -0500

    Print Names of Each Attendees' Legislators

--- full-stack-ruby-on-rails/ruby-exercises/event-manager/lib/event_manager.rb
+++ full-stack-ruby-on-rails/ruby-exercises/event-manager/lib/event_manager.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
   # Ensure all zipcodes a strings,
@@ -23,5 +27,20 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name)
+
+    legislators_string = legislator_names.join(', ')
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+
+  puts "#{name} #{zipcode} #{legislators_string}"
 end
