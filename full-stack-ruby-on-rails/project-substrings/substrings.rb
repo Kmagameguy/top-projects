e11d6888655f19dc226c9d2c5149f3f740654344
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:41:36 -0500

    Remove Notes & Pseudocode

--- full-stack-ruby-on-rails/project-substrings/substrings.rb
+++ full-stack-ruby-on-rails/project-substrings/substrings.rb
@@ -17,25 +17,6 @@ DICTIONARY = %w(
   sit
 ).freeze
 
-
-# Assignment: Return a hash which counts the number of times
-# any words from our dictionary appear as a substring of the user-provided word.
-#   - Make sure the evaluation is case-insensitive
-#   - Make sure it can evaluate multiple words
-#
-# To Do:
-# X - Downcase user input for easy comparison against the dictionary
-# X - Toss out special characters
-# X - Loop over the dictionary and count the number of times that the word appears
-# in the string
-#   - get the length of the current dictionary word
-#   - take user word and loop through the characters one-by-one, looking at a substring that is the length
-#     of the dictionary word.  Increment an index var until we reach the end of the string  Example: word[currentIndex, DICTIONARY_WORD.length]
-#   - Any time currentSubstring == DictionaryWord, increment that word's count and return to tracking hash
-# X - Revisit one of the past articles on enumerables/hashes/arrays -- there was a good example of creating
-# a key/pair with counted results (I think that was with .reduce())
-
-
 def substrings(string)
   cleaned_string = string.downcase.gsub(/[^A-Z]/i, '')
   matched_words = []
