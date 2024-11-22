86d3e24ffd6b8f7fc6c54492d1c4c52d72884972
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:39:03 -0500

    Complete 'Find The Oldest' Exercise

--- foundations/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ foundations/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    currentDate = (new Date()).getFullYear();
+    return people.reduce((oldest, nextPerson) => {
+        oldest.age = !oldest.yearOfDeath ? currentDate - oldest.yearOfBirth : oldest.yearOfDeath - oldest.yearOfBirth;
+        nextPerson.age = !nextPerson.yearOfDeath ? currentDate - nextPerson.yearOfBirth : nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+        return oldest.age > nextPerson.age ? oldest : nextPerson;
+    });
 };
 
 // Do not edit below this line
