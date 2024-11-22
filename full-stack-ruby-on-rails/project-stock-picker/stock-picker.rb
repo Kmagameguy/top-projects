bece85ed5b0f8ebb21cea7b9c405d9b7002c5ef0
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:41:45 -0500

    Show User The Best Day to Buy and Sell

--- full-stack-ruby-on-rails/project-stock-picker/stock-picker.rb
+++ full-stack-ruby-on-rails/project-stock-picker/stock-picker.rb
@@ -31,15 +31,18 @@ def stock_picker(prices)
   # found Array#combination from here:
   # https://ruby-doc.org/3.2.1/Array.html#method-i-combination
   # Ruby seems to have a method for everything
-  stonks = prices.combination(2).reduce(Hash.new({})) do |hash, price_group|
+  profit_matrix = prices.combination(2).reduce(Hash.new({})) do |hash, price_group|
     purchase_price = price_group[0]
     sale_price = price_group[1]
     profit = sale_price - purchase_price
-    hash[profit] = [purchase_price, sale_price]
+    hash[profit] = { :purchase_price => purchase_price, :sale_price => sale_price }
 
     hash
   end
-  p stonks
+
+  purchase_date = prices.index(profit_matrix.max[1][:purchase_price])
+  sale_date = prices.index(profit_matrix.max[1][:sale_price])
+  p [ purchase_date, sale_date ]
 end
 
 stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
