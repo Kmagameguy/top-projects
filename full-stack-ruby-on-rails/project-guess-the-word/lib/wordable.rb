9a6281e2b2ac4a4f408e7d6ead808e7c79eec95d
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:44:58 -0500

    Extract Word Length Boundaries to Constants

--- full-stack-ruby-on-rails/project-guess-the-word/lib/wordable.rb
+++ full-stack-ruby-on-rails/project-guess-the-word/lib/wordable.rb
@@ -3,6 +3,8 @@
 # This module manages our word selection
 module Wordable
   WORD_LIST = './assets/google-10000-english-no-swears.txt'
+  MIN_LENGTH = 5
+  MAX_LENGTH = 12
 
   def pick_word
     filtered_word_list.sample
@@ -12,7 +14,7 @@ module Wordable
 
   def filtered_word_list
     File.open(WORD_LIST).readlines.filter do |word|
-      word.chomp!.length.between?(5, 13)
+      word.chomp!.length.between?(MIN_LENGTH, MAX_LENGTH)
     end
   end
 end
