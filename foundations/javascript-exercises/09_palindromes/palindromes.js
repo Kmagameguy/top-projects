d9fdd9db55831487460e059056342d0eeb015dd6
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:38:38 -0500

    Complete 'Palindromes' Exercise

--- foundations/javascript-exercises/09_palindromes/palindromes.js
+++ foundations/javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (a) {
 
+    forwards = a.toLowerCase()
+            .replace(/[^a-zA-Z]/g, '')
+            .split('')
+            .join('');
+
+    reverse = a.toLowerCase()
+            .replace(/[^a-zA-Z]/g,'')
+            .split('')
+            .reverse()
+            .join('');
+
+    return forwards === reverse;
 };
 
 // Do not edit below this line
