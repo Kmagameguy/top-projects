96426cf8f578eb475d78f413cfe1ed8b782b6fe5
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:38:26 -0500

    Only Allow Valid Inputs Numbers only, values between 1 and 100

--- foundations/project-etch-a-sketch/js/script.js
+++ foundations/project-etch-a-sketch/js/script.js
@@ -1,6 +1,20 @@
 const GAME_BOARD = document.querySelector('#grid-container');
+const GRID_SIZE_BTN = document.querySelector('#change-grid');
 // 256 cells divides nicely into a 16x16 grid
 const CELL_COUNT = 256;
+const CELL_THRESHOLDS = [1, 100];
+
+function isBetween(num, min, max) {
+    return (num >= min && num <= max);
+}
+
+function changeGridSize() {
+    let userInput = parseInt(prompt('Set a new grid size:', 16));
+    if (!userInput || !isBetween(userInput, ...CELL_THRESHOLDS)) {
+        alert('Invalid option.  Input only accepts numbers between 1 and 100');
+        return
+    }
+}
 
 function highlightBox(e) {
     e.target.classList.toggle('marked');
@@ -12,3 +26,5 @@ for (let i = 0; i < CELL_COUNT; i++) {
         pixel.addEventListener('mouseover', highlightBox);
     GAME_BOARD.appendChild(pixel);
 }
+
+GRID_SIZE_BTN.addEventListener('click', changeGridSize);
