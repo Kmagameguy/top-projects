f7e10e80c9b0d1ec857ff5890c798e0cba1df129
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:41:54 -0500

    Rename bubble_sort Argument for Clarity

--- full-stack-ruby-on-rails/project-bubble-sort/bubble-sort.rb
+++ full-stack-ruby-on-rails/project-bubble-sort/bubble-sort.rb
@@ -1,21 +1,21 @@
-def bubble_sort(sequence)
-  max_iterations = sequence.length
+def bubble_sort(array)
+  max_iterations = array.length
 
   while max_iterations > 1 do
     index = 0
-    while index < sequence.length - 1 do
-      current_position = sequence[index]
-      next_position = sequence[index + 1]
+    while index < array.length - 1 do
+      current_position = array[index]
+      next_position = array[index + 1]
 
       if current_position > next_position
-        sequence[index] = next_position
-        sequence[index + 1] = current_position
+        array[index] = next_position
+        array[index + 1] = current_position
       end
       index += 1
     end
     max_iterations -= 1
   end
-  p sequence
+  p array
 end
 
 bubble_sort([4,3,78,2,0,2])
