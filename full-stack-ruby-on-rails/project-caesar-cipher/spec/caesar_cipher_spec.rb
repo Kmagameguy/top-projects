703b853c0e753f79ebda4a03c48b3cc4150b1b40
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:47:07 -0500

    Add Caesar Cipher Tests

--- full-stack-ruby-on-rails/project-caesar-cipher/spec/caesar_cipher_spec.rb
+++ full-stack-ruby-on-rails/project-caesar-cipher/spec/caesar_cipher_spec.rb
@@ -2,9 +2,26 @@
 
 require_relative '../lib/caesar_cipher'
 
-describe 'Caesar Cipher' do
+RSpec.describe CaesarCipher do
   describe '#encode' do
-    it 'correctly ciphers the string' do
+    it 'ensures case is preserved' do
+      c = CaesarCipher.new(text: 'sPonGeBob TeXt', key: 1)
+      expect(c.encode).to eq 'tQpoHfCpc UfYu'
+      expect(c.encode).to_not eq 'TQPOHFCPC UFYU'
+    end
+
+    it 'ensures letters wrap back to the beginning' do
+      c = CaesarCipher.new(text: 'ZZZZZZ', key: 3)
+      expect(c.encode).to eq 'CCCCCC'
+      expect(c.encode).to_not eq 'cccccc'
+    end
+
+    it 'does nothing to special characters and spaces' do
+      c = CaesarCipher.new(text:'!@#$%^&* ()', key: 7)
+      expect(c.encode).to eq '!@#$%^&* ()'
+    end
+
+    it 'correctly ciphers the given text' do
       c = CaesarCipher.new(text: 'What a string!', key: 5)
       expect(c.encode).to eq 'Bmfy f xywnsl!'
     end
