7fe04a6afe488187fcdaf9ea57ed880a2f253d84
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:38:06 -0500

    Complete 'Temperature Conversion' Exercise

--- foundations/javascript-exercises/07_tempConversion/tempConversion.js
+++ foundations/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(ftemp) {
+    let temp = (ftemp - 32) / 1.8;
+    return roundToOneDecimal(temp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ctemp) {
+    let temp = (ctemp * 1.8) + 32;
+    return roundToOneDecimal(temp);
 };
+
+function roundToOneDecimal(num) {
+    return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
