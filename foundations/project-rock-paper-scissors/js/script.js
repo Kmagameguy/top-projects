e0473d0e7d68ddd8d922210597697e906fcd2039
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:37:43 -0500

    Use Hash to Define Win Conditions

--- foundations/project-rock-paper-scissors/js/script.js
+++ foundations/project-rock-paper-scissors/js/script.js
@@ -3,6 +3,12 @@ console.log('get ready!');
 const CHOICES = ["rock", "paper", "scissors"];
 const NUM_ROUNDS = 5;
 
+const WIN_CONDITIONS = {
+    "rock": "scissors",
+    "paper": "rock",
+    "scissors": "paper"
+}
+
 function RoundResult(resultText, incrementScore) {
     this.resultText = resultText;
     this.incrementScore = incrementScore;
@@ -13,23 +19,12 @@ function getComputerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {
-    playerChoice = playerChoice.toLowerCase();
     if (playerChoice === computerChoice) {
         return new RoundResult("It's a draw!", false);
-    } else if (playerChoice === 'rock' && computerChoice === 'paper') {
-        return new RoundResult("You lose! Paper covers rock.", false);
-    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
-        return new RoundResult("You win! Rock smashes scissors", true);
-    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
-        return new RoundResult("You win! Paper covers rock.", true);
-    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
-        return new RoundResult("You lose! Scissors cut through paper.", false);
-    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
-        return new RoundResult("You lose! Rock smashes scissors.", false);
-    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
-        return new RoundResult("You win! Scissors cut through paper.", true);
+    } else if (WIN_CONDITIONS[playerChoice] === computerChoice) {
+        return new RoundResult("You win!", true);
     } else {
-        return new RoundResult("We didn't understand your guess. Try again.", false);
+        return new RoundResult("You lose!", false);
     }
 }
 
