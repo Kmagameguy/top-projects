50be55e312ef3d1f874183b2aada36ed7c043d82
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:44:03 -0500

    Extract Thank You Letter Generation to Method

--- full-stack-ruby-on-rails/ruby-exercises/event-manager/lib/event_manager.rb
+++ full-stack-ruby-on-rails/ruby-exercises/event-manager/lib/event_manager.rb
@@ -17,13 +17,23 @@ def legislators_by_zipcode(zipcode)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zipcode,
       levels: 'country',
       roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
   end
 end
 
@@ -48,11 +58,5 @@ contents.each do |row|
 
   form_letter = erb_template.result(binding)
 
-  Dir.mkdir('output') unless Dir.exist?('output')
-
-  filename = "output/thanks_#{id}.html"
-
-  File.open(filename, 'w') do |file|
-    file.puts form_letter
-  end
+  save_thank_you_letter(id, form_letter)
 end
