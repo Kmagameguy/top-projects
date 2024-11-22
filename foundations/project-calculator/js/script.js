c7ca89719ac3371488b2a14092c1c8cf0f06ad75
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:39:22 -0500

    Create the Worst Logic of All Time

--- foundations/project-calculator/js/script.js
+++ foundations/project-calculator/js/script.js
@@ -3,7 +3,9 @@ console.log('ready!');
 const BUTTONS = document.querySelectorAll('button');
 const CALCULATOR_DISPLAY = document.querySelector('#calculator-text');
 
-let displayValue = 0;
+let firstNum = null;
+let operator = null;
+let secondNum = null;
 
 function addNumbers(num1, num2) {
     return num1 + num2;
@@ -22,17 +24,33 @@ function divideNumbers(num1, num2) {
     else { return num1 / num2 }
 }
 
-function appendToDisplay(number) {
-    displayValue = CALCULATOR_DISPLAY.innerText;
-    CALCULATOR_DISPLAY.innerText = displayValue === '0' ? number : displayValue += number;
+function isNumber(value) {
+    return !isNaN(parseInt(value));
+}
+
+function updateDisplay(number) {
+    CALCULATOR_DISPLAY.innerText = number;
+}
+
+function updateOrAppendToDisplay(number) {
+    let currentDisplay = CALCULATOR_DISPLAY.innerText;
+    currentDisplay === '0' ? updateDisplay(number) : updateDisplay(currentDisplay + number);
 }
 
 function clearDisplay() {
     CALCULATOR_DISPLAY.innerText = '0';
-    displayValue = 0;
+    firstNum = null;
+    operator = null;
+    secondNum = null;
 }
 
-function operate(num1, num2, operator) {
+function shiftMemory() {
+    firstNum = parseInt(CALCULATOR_DISPLAY.innerText);
+    operator = null;
+    secondNum = null;
+}
+
+function operate(num1, operator, num2) {
     let result = '';
     switch(operator) {
         case '+':
@@ -54,17 +72,46 @@ function operate(num1, num2, operator) {
 function handleInput(e) {
     let selectedButton = e.target.innerText;
 
-    if (!isNaN(parseInt(selectedButton))) {
-        appendToDisplay(selectedButton);
-    } else if (selectedButton === 'AC') {
+    if (selectedButton === 'AC') {
         clearDisplay();
+    } else if (selectedButton === '=') {
+        // We have to handle = separately since it can't be chained like other operators
+        if (firstNum !== null && operator !== null && secondNum !== null) {
+            secondNum = parseInt(CALCULATOR_DISPLAY.innerText);
+            updateDisplay(operate(firstNum, operator, secondNum));
+            firstNum = null;
+            operator = null;
+            secondNum = null;
+        }
+    } else if (firstNum === null && operator === null && secondNum === null) {
+        if (isNumber(selectedButton)) {
+            updateOrAppendToDisplay(selectedButton);
+        } else {
+            firstNum = parseInt(CALCULATOR_DISPLAY.innerText);
+            operator = selectedButton;
+        }
+    // this condition is really only here to handle the display.  We want to show the previously
+    // entered number until the user starts clicking buttons again.  After the first keypress we
+    // wipe the display and show the newly entered number.  This condition sets the secondNum
+    // to a value other than null so that it short circuits the next time around and
+    // continues to the third else if statement in this chain
+    } else if (firstNum !== null && operator !== null && secondNum === null) {
+        if (isNumber(selectedButton)) {
+            updateDisplay(selectedButton);
+            secondNum = parseInt(selectedButton);
+        }
+    // this condition is what helps us chain operators together.  if the list of operations is full
+    // we execute the stored stuff and then shift the values around so we're ready to take the next input
+    } else if (firstNum !== null && operator !== null && secondNum !== null) {
+        if (isNumber(selectedButton)) {
+            updateOrAppendToDisplay(selectedButton);
+        } else {
+            secondNum = parseInt(CALCULATOR_DISPLAY.innerText);
+            updateDisplay(operate(firstNum, operator, secondNum));
+            shiftMemory();
+            operator = selectedButton;
+        }
     }
-
-    console.log(operate(1, 3, '+'));
-    console.log(operate(3, 1, '-'));
-    console.log(operate(2, 3, '*'));
-    console.log(operate(10, 5, '/'));
 }
 
-CALCULATOR_DISPLAY.innerText = displayValue;
 BUTTONS.forEach(button => button.addEventListener('click',handleInput));
