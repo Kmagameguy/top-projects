58a924d23b8f7bb2a54c1e32d9ae17c52c372c4c
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:43:46 -0500

    List Attendee Names

--- full-stack-ruby-on-rails/ruby-exercises/event-manager/lib/event_manager.rb
+++ full-stack-ruby-on-rails/ruby-exercises/event-manager/lib/event_manager.rb
@@ -1,1 +1,16 @@
+# frozen_string_literal: true
+
+require 'csv'
 puts 'Event Manager Initialized!'
+
+filename = 'event_attendees.csv'
+contents = CSV.open(
+  filename,
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  puts name
+end
