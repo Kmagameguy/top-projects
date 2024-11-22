7e251f2fb4f954374ad81d1b202a7a7a437a8430
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:43:53 -0500

    Move Zipcode Cleaning to Method

--- full-stack-ruby-on-rails/ruby-exercises/event-manager/lib/event_manager.rb
+++ full-stack-ruby-on-rails/ruby-exercises/event-manager/lib/event_manager.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 require 'csv'
+
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    zipcode = '00000'
+  elsif zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  else
+    zipcode
+  end
+end
+
 puts 'Event Manager Initialized!'
 
 filename = 'event_attendees.csv'
@@ -12,15 +25,7 @@ contents = CSV.open(
 
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
-
-  if zipcode.nil?
-    zipcode = '00000'
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust(5, '0')
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  end
+  zipcode = clean_zipcode(row[:zipcode])
 
   puts "#{name} #{zipcode}"
 end
