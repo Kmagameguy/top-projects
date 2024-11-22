edd9a0aac3b13a178a5b8b670d9c63ca152f9554
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:38:03 -0500

    Complete 'Sum All' Exercise

--- foundations/javascript-exercises/05_sumAll/sumAll.js
+++ foundations/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(startingNum, endingNum) {
 
+    if (startingNum < 0 || endingNum < 0) return 'ERROR'
+    if (typeof startingNum !== 'number' || typeof endingNum !== 'number') return 'ERROR'
+
+    nums = [startingNum, endingNum].sort();
+    let sumTotal = 0;
+
+    for (let i = nums[0]; i <= nums[1]; i++) {
+        sumTotal += i;
+    }
+
+    return sumTotal;
 };
 
 // Do not edit below this line
