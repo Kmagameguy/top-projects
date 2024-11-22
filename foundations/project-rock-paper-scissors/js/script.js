5736eb98c7f371036fe9137ebddf079802cd3200
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:37:21 -0500

    Make Computer Pick a Hand

--- foundations/project-rock-paper-scissors/js/script.js
+++ foundations/project-rock-paper-scissors/js/script.js
@@ -1,1 +1,9 @@
 console.log('get ready!');
+
+const CHOICES = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  return CHOICES[Math.floor(Math.random() * CHOICES.length )];
+}
+
+console.log(`Computer chose: ${getComputerChoice()}`);
