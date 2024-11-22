9c0ff0f0d6873fe3ef15edff728dd06cc7f0c668
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:41:15 -0500

    Map Nums Back to Characters

--- full-stack-ruby-on-rails/project-caesar-cipher/caesar-cipher.rb
+++ full-stack-ruby-on-rails/project-caesar-cipher/caesar-cipher.rb
@@ -9,9 +9,39 @@
 # X - Map each character to its position in the alphabet
 #    X - (ignoring special characters)
 # X - Add the offset to each character (and wrap if above 26)
-# Map each number back to its corresponding character
+# X - Map each number back to its corresponding character
 # Join back into a string
 # Keep the casing consistent
+
+ALPHA_MAP = {
+  0 =>  'A',
+  1 =>  'B',
+  2 =>  'C',
+  3 =>  'D',
+  4 =>  'E',
+  5 =>  'F',
+  6 =>  'G',
+  7 =>  'H',
+  8 =>  'I',
+  9 =>  'J',
+  10 => 'K',
+  11 => 'L',
+  12 => 'M',
+  13 => 'N',
+  14 => 'O',
+  15 => 'P',
+  16 => 'Q',
+  17 => 'R',
+  18 => 'S',
+  19 => 'T',
+  20 => 'U',
+  21 => 'V',
+  22 => 'W',
+  23 => 'X',
+  24 => 'Y',
+  25 => 'Z'
+}
+
 
 def map_to_num(char)
   # Upcase and subtract 65 to shift the ordinal number index
@@ -38,12 +68,18 @@ def shift(char_index, offset)
   return (offset - (last_index_of_alphabet - char_index)) - 1
 end
 
+def map_to_char(num)
+  return num if !num.is_a?(Integer)
+  return ALPHA_MAP[num]
+end
+
 def caesar_cipher(text, offset)
   arr = text.split('')
-  arr.map do |char|
-      puts "Character #{char} current index is: #{map_to_num(char)}"
-      puts "Character #{char} new index is: #{shift(map_to_num(char), offset)}"
+  shifted_arr = arr.map do |char|
+      map_to_char(shift(map_to_num(char), offset))
   end
+
+  p shifted_arr
 end
 
 caesar_cipher('What a string!', 5)
