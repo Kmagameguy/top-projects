2cb16fcacd9525cf8bdbededde996960487ab9f4
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:38:36 -0500

    Complete 'Calculator' Exercise

--- foundations/javascript-exercises/08_calculator/calculator.js
+++ foundations/javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+    return a.reduce((total,current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+    return a.flat().reduce((total, value) => {
+        return total * value;
+    },1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    let factors = [];
+    for (let i = a; i > 0; i--) {
+        factors.push(i);
+    }
+
+    return factors.reduce((total, current) => total * current, 1);
+
 };
 
 // Do not edit below this line
