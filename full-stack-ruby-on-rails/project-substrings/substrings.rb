552b2f669585142b2b8593785e9ef2873fbb8abd
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:41:35 -0500

    Count Matched Substrings and Add to Hash

--- full-stack-ruby-on-rails/project-substrings/substrings.rb
+++ full-stack-ruby-on-rails/project-substrings/substrings.rb
@@ -26,24 +26,33 @@ DICTIONARY = %w(
 # To Do:
 # X - Downcase user input for easy comparison against the dictionary
 # X - Toss out special characters
-# Loop over the dictionary and count the number of times that the word appears
+# X - Loop over the dictionary and count the number of times that the word appears
 # in the string
 #   - get the length of the current dictionary word
 #   - take user word and loop through the characters one-by-one, looking at a substring that is the length
 #     of the dictionary word.  Increment an index var until we reach the end of the string  Example: word[currentIndex, DICTIONARY_WORD.length]
 #   - Any time currentSubstring == DictionaryWord, increment that word's count and return to tracking hash
-# Revisit one of the past articles on enumerables/hashes/arrays -- there was a good example of creating
+# X - Revisit one of the past articles on enumerables/hashes/arrays -- there was a good example of creating
 # a key/pair with counted results (I think that was with .reduce())
 
 
 def substrings(string)
   cleaned_string = string.downcase.gsub(/[^A-Z]/i, '')
-  DICTIONARY.each do |word|
+  matched_words = []
+
+  DICTIONARY.map do |word|
     cleaned_string.chars.each_with_index do |char, index|
-      temp_str = cleaned_string[index, word.length]
-      puts "Match found: #{word} in #{temp_str}" if temp_str == word
+      substring = cleaned_string[index, word.length]
+      matched_words.push(substring) if substring == word
     end
   end
+
+  matched_words_with_counts = matched_words.reduce(Hash.new(0)) do |hash, key|
+    hash[key] += 1
+    hash
+  end
+
+  puts matched_words_with_counts
 end
 
 substrings("Howdy partner, sit down! How's it going?")
