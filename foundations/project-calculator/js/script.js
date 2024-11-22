5571785a2493bf4158f43b5526d3edce0e3c0f03
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:39:11 -0500

    Reflect Inputs in Display

--- foundations/project-calculator/js/script.js
+++ foundations/project-calculator/js/script.js
@@ -1,6 +1,9 @@
 console.log('ready!');
 
 const BUTTONS = document.querySelectorAll('button');
+const CALCULATOR_DISPLAY = document.querySelector('#calculator-text');
+
+let displayValue = 0;
 
 function addNumbers(num1, num2) {
     return num1 + num2;
@@ -17,6 +20,11 @@ function multiplyNumbers(num1, num2) {
 function divideNumbers(num1, num2) {
     if (num2 === 0) { return 'Divide by Zero Error'}
     else { return num1 / num2 }
+}
+
+function appendToDisplay(number) {
+    let currentDisplay = CALCULATOR_DISPLAY.innerText;
+    CALCULATOR_DISPLAY.innerText = currentDisplay === '0' ? number : currentDisplay += number;
 }
 
 function operate(num1, num2, operator) {
@@ -39,10 +47,17 @@ function operate(num1, num2, operator) {
 }
 
 function handleInput(e) {
+    let selectedButton = e.target.innerText;
+
+    if (!isNaN(parseInt(selectedButton))) {
+        appendToDisplay(selectedButton);
+    }
+
     console.log(operate(1, 3, '+'));
     console.log(operate(3, 1, '-'));
     console.log(operate(2, 3, '*'));
     console.log(operate(10, 5, '/'));
 }
 
+CALCULATOR_DISPLAY.innerText = displayValue;
 BUTTONS.forEach(button => button.addEventListener('click',handleInput));
