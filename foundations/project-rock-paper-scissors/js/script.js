3991684312edf8e33dc03eaf95bc20401607a5c6
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:37:25 -0500

    Handle All Win or Lose Scenarios

--- foundations/project-rock-paper-scissors/js/script.js
+++ foundations/project-rock-paper-scissors/js/script.js
@@ -10,6 +10,18 @@ function playRound(playerChoice, computerChoice) {
   playerChoice = playerChoice.toLowerCase();
   if (playerChoice === computerChoice) {
     return "It's a draw!";
+  } else if (playerChoice === 'rock' && computerChoice === 'paper') {
+    return "You lose!  Paper covers rock.";
+  } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+    return "You win!  Rock smashes scissors.";
+  } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+    return "You win!  Paper covers rock.";
+  } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+    return "You lose!  Scissors cut through paper.";
+  } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+    return "You lose!  Rock smashes scissors.";
+  } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+    return "You win!  Scissors cut through paper.";
   } else {
     return "Something else happened...";
   }
@@ -18,4 +30,5 @@ function playRound(playerChoice, computerChoice) {
 const playerChoice = 'rock';
 const computerChoice = getComputerChoice();
 console.log(`Computer chose: ${computerChoice}`);
+console.log(`You chose: ${playerChoice}`);
 console.log(playRound(playerChoice, computerChoice));
