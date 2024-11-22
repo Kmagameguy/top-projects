9f99ea06b1741014aabb20584c7643ad58266f8d
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:37:58 -0500

    Complete 'Repeat String' Exericse

--- foundations/javascript-exercises/02_repeatString/repeatString.js
+++ foundations/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, repeatCount) {
+    if (repeatCount < 0) { return 'ERROR'}
 
+    let returnString = '';
+
+    for (i = 0; i < repeatCount; i++) {
+        returnString += string;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
