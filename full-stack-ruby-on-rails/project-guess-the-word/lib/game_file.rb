d05098cb6f6be72c6536191e355ed90b0d873f90
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 18:44:48 -0500

    Dedicate a Directory to Saved Game Files

--- full-stack-ruby-on-rails/project-guess-the-word/lib/game_file.rb
+++ full-stack-ruby-on-rails/project-guess-the-word/lib/game_file.rb
@@ -1,17 +1,26 @@
 class GameFile
 
   DEFAULT_SAVE_FILE = 'saved.yml'
+  DEFAULT_SAVE_LOCATION = './saves'
 
   def initialize(data = {})
     puts 'Game saved.'
 
-    save_file = File.open(DEFAULT_SAVE_FILE, 'w')
+    ensure_save_directory_exists
+
+    save_file = File.open("#{DEFAULT_SAVE_LOCATION}/#{DEFAULT_SAVE_FILE}", 'w')
     save_file.puts YAML.dump(data)
     save_file.close
   end
 
   def self.load
     puts 'Previous game restored.'
-    YAML.safe_load(File.open(DEFAULT_SAVE_FILE), permitted_classes: [Symbol])
+    YAML.safe_load(File.open("#{DEFAULT_SAVE_LOCATION}/#{DEFAULT_SAVE_FILE}"), permitted_classes: [Symbol])
+  end
+
+  private
+
+  def ensure_save_directory_exists
+    Dir.mkdir(DEFAULT_SAVE_LOCATION) unless Dir.exist?(DEFAULT_SAVE_LOCATION)
   end
 end
