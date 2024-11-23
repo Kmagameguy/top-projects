5ce652f0b1cea6654844fd1f5be57b9e8514cbd8
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:19:27 -0500

    Rough Out Setting and Getting Board Values

--- full-stack-ruby-on-rails/project-connect-four/lib/board.rb
+++ full-stack-ruby-on-rails/project-connect-four/lib/board.rb
@@ -2,12 +2,19 @@
 
 # A class to manage our Connect Four game
 class Board
+  attr_accessor :slots
+
   def initialize
     @rows = 6
     @columns = 7
+    @slots = Array.new(@rows) { Array.new(@columns) }
   end
 
   def size
     { rows: @rows, columns: @columns }
+  end
+
+  def find_slot(row, column)
+    @slots[row - 1][column - 1]
   end
 end
--- full-stack-ruby-on-rails/project-connect-four/spec/board_spec.rb
+++ full-stack-ruby-on-rails/project-connect-four/spec/board_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe Board do
       expect(board.size).to eq({ rows: 6, columns: 7})
     end
   end
+
+  describe '#find_slot' do
+    it 'returns the value of the slot at the given coordinates' do
+      board.slots[2][5] = 'x'
+      expect(board.find_slot(3, 6)).to eql 'x'
+    end
+  end
 end
