700ac1cdf7126f8ef6fa468f576bf6126bc720df
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:24:49 -0500

    Allow Searching by Flight Dates

--- full-stack-ruby-on-rails/project-flight-booker/app/controllers/flights_controller.rb
+++ full-stack-ruby-on-rails/project-flight-booker/app/controllers/flights_controller.rb
@@ -1,16 +1,18 @@
 class FlightsController < ApplicationController
   def index
     @all_flights = Flight.all
+    @departure_times = Flight.upcoming_flight_times
 
     unless params[:arrival_airport_id].nil?
       @search_results = Flight.where(departure_airport_id: params[:departure_airport_id])
                               .where(arrival_airport_id: params[:arrival_airport_id])
+                              .select { |f| f.friendly_date == params[:departure_time] }
     end
   end
 
   private
 
   def flight_params
-    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id)
+    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :departure_time)
   end
 end
--- full-stack-ruby-on-rails/project-flight-booker/app/models/flight.rb
+++ full-stack-ruby-on-rails/project-flight-booker/app/models/flight.rb
@@ -1,4 +1,19 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport", primary_key: "code"
   belongs_to :arrival_airport, class_name: "Airport", primary_key: "code"
+
+  scope :upcoming_flight_times, lambda {
+    where("departure_time > ?", Time.now)
+      .order(departure_time: :asc)
+      .map { |flight| flight.friendly_date }
+      .uniq
+  }
+
+  def friendly_date
+    departure_time.strftime("%b %e, %Y")
+  end
+
+  def friendly_datetime
+    departure_time.strftime("%b %e, %Y at %l:%M %p")
+  end
 end
