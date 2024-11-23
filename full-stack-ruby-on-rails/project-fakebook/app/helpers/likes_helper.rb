0997efc011efa9b5c92e8991f6cd52c05501f78e
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:26:50 -0500

    Fix Likes & Show Who Liked a Commentable Object

--- full-stack-ruby-on-rails/project-fakebook/app/helpers/likes_helper.rb
+++ full-stack-ruby-on-rails/project-fakebook/app/helpers/likes_helper.rb
@@ -1,5 +1,9 @@
 module LikesHelper
   def already_liked?(likeable)
-    current_user.likes.any? { |like| like.likeable_id == likeable.id }
+    current_user.likes.any? { |like| likeable.likes.pluck(:user_id).include?(like.likeable_id) }
+  end
+
+  def liked_by(likeable)
+    likeable.likes.pluck(:user_id).map { |id| User.find(id).name }.join(', ')
   end
 end
