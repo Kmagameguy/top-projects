376c1bcfe081e61da23e920a1ddb04c2210a3f47
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:23:27 -0500

    Allow Searches by Flickr Username

--- full-stack-ruby-on-rails/project-photo-stream/app/controllers/static_pages_controller.rb
+++ full-stack-ruby-on-rails/project-photo-stream/app/controllers/static_pages_controller.rb
@@ -1,5 +1,19 @@
 class StaticPagesController < ApplicationController
   def index
-    @photos = flickr.interestingness.getList(per_page: 10).photos
+    @photos = if params[:username].present?
+                flickr.people.getPublicPhotos(user_id: user_id(params[:username]))
+              else
+                flickr.interestingness.getList(per_page: 10).photos
+              end
+  end
+
+  private
+
+  def user_id(username)
+    flickr.people.findByUsername(username:).nsid
+  end
+
+  def search_params
+    params.permit(:username)
   end
 end
