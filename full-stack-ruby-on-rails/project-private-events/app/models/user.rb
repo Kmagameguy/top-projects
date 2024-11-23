3e8b70be43421a94bae9a5962cf03628b190cc41
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:23:50 -0500

    Ensure Events Owned by Deleted User are Also Deleted

--- full-stack-ruby-on-rails/project-private-events/app/models/user.rb
+++ full-stack-ruby-on-rails/project-private-events/app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
 
-  has_many :created_events, foreign_key: :creator_id, class_name: "Event"
+  has_many :created_events, foreign_key: :creator_id, class_name: "Event", dependent: :destroy
 end
