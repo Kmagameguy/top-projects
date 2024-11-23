f57e8dacb23e676204a3161c7204109e0dcf4f85
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:16:21 -0500

    Generate List of Possible Squares

--- full-stack-ruby-on-rails/project-knights-travails/lib/knight.rb
+++ full-stack-ruby-on-rails/project-knights-travails/lib/knight.rb
@@ -2,13 +2,26 @@
 
 # A class which defines our Knight chess piece
 class Knight
-  MOVES = [[-1, -2], [-2, -1], [-2, 1], [-1, 2],
-           [1, -2],  [2, -1],  [2, 1],  [1, 2]].freeze
+  attr_accessor :coordinates
 
-  def initialize; end
+  MOVES = [[-1, -2], [1, -2], [2, -1], [2, 1],
+           [1, 2],  [-1, 2],  [-2, 1],  [-2, -1]].freeze
+
+  def initialize(starting_position = [0, 0])
+    @coordinates = starting_position
+  end
+
+  def possible_next_moves
+    x, y = @coordinates
+    MOVES.map { |x_offset, y_offset| [x + x_offset, y + y_offset] }
+  end
 
   def knight_moves; end
 end
+
+k = Knight.new([3, 4])
+puts "Starting position: #{k.coordinates}"
+p k.possible_next_moves
 
 # Valid moves (Where "0, 0" is current position):
 #       0        1        2        3        4        5        6        7
