cf81fb0ce438b8ab8ff1d59114589ff2e8a69f46
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:23:55 -0500

    Add Character Length Restrictions to Event Titles & Descriptions

--- full-stack-ruby-on-rails/project-private-events/app/models/event.rb
+++ full-stack-ruby-on-rails/project-private-events/app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
+  validates :title, presence: true, length: { maximum: 75 }
+  validates :description, presence: true, length: { maximum: 140 }
   validates :location, presence: true
   validates :event_date, presence: true
 
