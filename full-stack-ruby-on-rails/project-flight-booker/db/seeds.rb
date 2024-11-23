4774793d261c5cf71e7bf47615235794cc7b6747
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:24:37 -0500

    Seed Database

--- full-stack-ruby-on-rails/project-flight-booker/db/seeds.rb
+++ full-stack-ruby-on-rails/project-flight-booker/db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+AIRPORT_CODES = %w[
+  ATL
+  DFW
+  DEN
+  ORD
+  LAX
+  CLT
+  MCO
+  LAS
+  PHX
+  MIA
+  SEA
+  IAH
+  JFK
+  EWR
+  FLL
+  MSP
+  SFO
+  DTW
+  BOS
+  SLC
+  PHL
+  BWI
+  TPA
+  SAN
+  LGA
+  MDW
+  BNA
+  IAD
+  DCA
+  AUS
+].freeze
+
+Airport.destroy_all
+Flight.destroy_all
+Airport.create!(AIRPORT_CODES.map { |code| { code: } })
+
+puts "Created #{Airport.count} airports."
+
+(AIRPORT_CODES.count - 1).times do |i|
+  Flight.create!(departure_airport_id: AIRPORT_CODES.first, arrival_airport_id: AIRPORT_CODES[i + 1])
+end
+
+puts "Created #{Flight.count} flights."
