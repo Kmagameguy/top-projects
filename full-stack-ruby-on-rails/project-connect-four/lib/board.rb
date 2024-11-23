a7b9b8f66da5e844832af5a6a73d647d1208565e
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:19:25 -0500

    Start Roughing Out the Game Board

--- full-stack-ruby-on-rails/project-connect-four/lib/board.rb
+++ full-stack-ruby-on-rails/project-connect-four/lib/board.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 # A class to manage our Connect Four game
-class Board; end
+class Board
+  def initialize
+    @rows = 6
+    @columns = 7
+  end
+
+  def size
+    { rows: @rows, columns: @columns }
+  end
+end
--- full-stack-ruby-on-rails/project-connect-four/spec/board_spec.rb
+++ full-stack-ruby-on-rails/project-connect-four/spec/board_spec.rb
@@ -3,4 +3,10 @@
 require 'board'
 
 RSpec.describe Board do
+  subject(:board) { Board.new }
+  describe '#size' do
+    it 'should have a grid size of 7x6 squares' do
+      expect(board.size).to eq({ rows: 6, columns: 7})
+    end
+  end
 end
