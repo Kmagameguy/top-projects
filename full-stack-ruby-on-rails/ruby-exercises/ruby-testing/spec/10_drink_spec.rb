19c3db0646227dbb7dbaf22df40d5a4dccc78157
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:18:15 -0500

    Complete '10 Drink' Exercise

--- full-stack-ruby-on-rails/ruby-exercises/ruby-testing/spec/10_drink_spec.rb
+++ full-stack-ruby-on-rails/ruby-exercises/ruby-testing/spec/10_drink_spec.rb
@@ -91,13 +91,16 @@ describe Drink do
     context 'when type is specified and ounces is default' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
+      subject(:my_beverage) { described_class.new('pepsi') }
 
       # remove the 'x' before running this test
-      xit 'is your choice of beverage' do
+      it 'is your choice of beverage' do
+        expect(my_beverage.type).to eq 'pepsi'
       end
 
       # remove the 'x' before running this test
-      xit 'has 16 ounces' do
+      it 'has 16 ounces' do
+        expect(my_beverage).to be_full
       end
     end
   end
@@ -106,18 +109,21 @@ describe Drink do
     context 'when drink has 16 ounces or more' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
+      subject(:my_beverage) { described_class.new('pepsi', 24) }
 
       # remove the 'x' before running this test
-      xit 'is full' do
+      it 'is full' do
+        expect(my_beverage).to be_full
       end
     end
 
     context 'when drink has less than 16 ounces' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type. In addition, specify ounces to be any number under 16.
-
+      subject(:my_beverage) { described_class.new('pepsi', 15) }
       # remove the 'x' before running this test
-      xit 'is not full' do
+      it 'is not full' do
+        expect(my_beverage).to_not be_full
       end
     end
   end
