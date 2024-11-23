30b08db9f4e0f3787ae1d48db5f0bb38dd2e47a8
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:25:00 -0500

    Add More Constraints to DB Tables

--- full-stack-ruby-on-rails/project-flight-booker/db/migrate/20230619134232_create_airports.rb
+++ full-stack-ruby-on-rails/project-flight-booker/db/migrate/20230619134232_create_airports.rb
@@ -1,7 +1,7 @@
 class CreateAirports < ActiveRecord::Migration[7.0]
   def change
     create_table :airports, id: false do |t|
-      t.string :code
+      t.string :code, null: false, primary_key: true
 
       t.timestamps
     end
--- full-stack-ruby-on-rails/project-flight-booker/db/migrate/20230619135622_create_flights.rb
+++ full-stack-ruby-on-rails/project-flight-booker/db/migrate/20230619135622_create_flights.rb
@@ -1,8 +1,8 @@
 class CreateFlights < ActiveRecord::Migration[7.0]
   def change
     create_table :flights do |t|
-      t.string :departure_airport_id
-      t.string :arrival_airport_id
+      t.string :departure_airport_id, null: false
+      t.string :arrival_airport_id, null: false
 
       t.timestamps
     end
