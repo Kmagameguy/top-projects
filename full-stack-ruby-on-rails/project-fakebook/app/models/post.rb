63a443f25a92f02d7ba444027ecee69b8945a643
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:26:09 -0500

    Load Posts Newest First

--- full-stack-ruby-on-rails/project-fakebook/app/models/post.rb
+++ full-stack-ruby-on-rails/project-fakebook/app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
 
   belongs_to :author, class_name: "User", foreign_key: :user_id
+
+  default_scope { order(created_at: :desc) }
 end
