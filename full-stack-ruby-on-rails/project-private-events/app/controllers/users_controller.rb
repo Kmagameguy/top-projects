22b9a18d2a4d59a84925a6a4201a9eca9f2d8575
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:24:19 -0500

    Don't Let Users See Others' Subbed Events

--- full-stack-ruby-on-rails/project-private-events/app/controllers/users_controller.rb
+++ full-stack-ruby-on-rails/project-private-events/app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @user = User.find(params[:id])
+
+    if current_user.id != @user.id
+      flash[:alert] = 'Access denied.'
+      redirect_to root_path
+    end
   end
 end
