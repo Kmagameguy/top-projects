3a19f6ecc8e8617654e91180cf926668ab5f023c
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:25:27 -0500

    Publish & Rebroadcast Messages to Connected Clients

--- full-stack-ruby-on-rails/rails-exercises/messenger/app/javascript/channels/message_channel.js
+++ full-stack-ruby-on-rails/rails-exercises/messenger/app/javascript/channels/message_channel.js
@@ -1,6 +1,6 @@
 import consumer from "channels/consumer"
 
-consumer.subscriptions.create("MessageChannel", {
+const messageChannel = consumer.subscriptions.create("MessageChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
   },
@@ -10,6 +10,36 @@ consumer.subscriptions.create("MessageChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    const messageDisplay = document.querySelector('#message-display')
+    messageDisplay.insertAdjacentHTML('beforeend', this.template(data))
+  },
+
+  template(data) {
+    return `<article class="message">
+                <div class="message-header">
+                    <p>${data.user.email}</p>}
+                </div>
+                <div class="message-body">
+                    <p>${data.message.body}</p>
+                </div>
+            </article>`
   }
 });
+
+document.addEventListener("turbo:load", () => {
+    let form = document.querySelector("#message-form");
+    if (form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const messageInput = document.querySelector("#message-input");
+            if (messageInput.value == '') return;
+
+            const message = {
+                body: messageInput.value
+            }
+
+            messageInput.value = '';
+            messageChannel.send({ message: message });
+        })
+    }
+})
