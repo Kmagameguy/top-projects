2e39041829d5394fd9e41f8c43b1e8706306c482
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:24:40 -0500

    Search by Departure & Arrival Airports

--- full-stack-ruby-on-rails/project-flight-booker/app/controllers/flights_controller.rb
+++ full-stack-ruby-on-rails/project-flight-booker/app/controllers/flights_controller.rb
@@ -1,5 +1,16 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    @all_flights = Flight.all
+
+    unless params[:arrival_airport_id].nil?
+      @search_results = Flight.where(departure_airport_id: params[:departure_airport_id])
+                              .where(arrival_airport_id: params[:arrival_airport_id])
+    end
+  end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id)
   end
 end
