28675c6abc70b0c19d2dea3add07db824f5afa3f
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:21:07 -0500

    Fix Pawn Movement Bug

--- full-stack-ruby-on-rails/project-chess/lib/pieces/pawn.rb
+++ full-stack-ruby-on-rails/project-chess/lib/pieces/pawn.rb
@@ -7,12 +7,12 @@ class Pawn < Piece
   ONE_STEP = [1, 0].freeze
   TWO_STEP = [2, 0].freeze
 
-  def possible_moves(board)
+  def possible_moves(squares)
     x, y = position
     move_set.map { |offset_x, offset_y| [x + offset_x, y + offset_y] }
-            .reject { |move| blocked?(move, board) }
-            .concat(attackable_squares(board))
-            .reject { |move| out_of_bounds?(move, board) }
+            .reject { |move| blocked?(move, squares) }
+            .concat(attackable_squares(squares))
+            .reject { |move| out_of_bounds?(move, squares) }
   end
 
   def move_set
@@ -21,25 +21,36 @@ class Pawn < Piece
     moves
   end
 
-  def attackable_squares(board)
-    squares = []
+  def attackable_squares(squares)
+    attackable_squares = []
+
     l_rank, l_file = left_diag
     r_rank, r_file = right_diag
 
-    squares << left_diag unless board[l_rank][l_file].nil?
-    squares << right_diag unless board[r_rank][r_file].nil?
+    attackable_squares << left_diag unless squares[l_rank][l_file].nil?
+    attackable_squares << right_diag unless squares[r_rank][r_file].nil?
 
-    squares
+    attackable_squares
   end
 
   def left_diag
     rank, file = position
-    [rank + 1, file - 1]
+
+    if color == :white
+      [rank - 1, file - 1]
+    else
+      [rank + 1, file - 1]
+    end
   end
 
   def right_diag
     rank, file = position
-    [rank + 1, file + 1]
+
+    if color == :white
+      [rank - 1, file + 1]
+    else
+      [rank + 1, file + 1]
+    end
   end
 
   def invert(moves)
