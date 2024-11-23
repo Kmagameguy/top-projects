d868aab443d0952358475572014a4bb47edf38b3
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:17:28 -0500

    Fix Knight Tests

--- full-stack-ruby-on-rails/project-knights-travails/spec/knight_spec.rb
+++ full-stack-ruby-on-rails/project-knights-travails/spec/knight_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../lib/knight'
+require_relative '../lib/node'
 
 RSpec.describe 'Knight' do
   describe 'creating Knights' do
@@ -10,26 +11,29 @@ RSpec.describe 'Knight' do
     end
 
     it 'creates a Knight at the coordinate [3, 5]' do
-      k = Knight.new([3, 5])
+      k = Knight.new(starting_position: [3, 5])
       expect(k.coordinates).to eq [3, 5]
     end
   end
 
   describe 'moving the Knight' do
     it 'creates a clockwise 2d array of next-possible moves for the Knight' do
-      k = Knight.new([3, 4])
-      expect(k.possible_next_moves).to eq [[2, 2], [4, 2], [5, 3], [5, 5], [4, 6], [2, 6], [1, 5], [1, 3]]
+      k = Knight.new(starting_position: [3, 4])
+      expect(k.possible_next_moves([])).to eq [[2, 2], [4, 2], [5, 3], [5, 5], [4, 6], [2, 6], [1, 5], [1, 3]]
     end
 
     it 'discards any moves that would place the Knight off the board' do
-      k = Knight.new([0, 7])
-      expect(k.possible_next_moves).to eq [[1, 5], [2, 6]]
+      k = Knight.new(starting_position: [0, 7])
+      expect(k.possible_next_moves([])).to eq [[1, 5], [2, 6]]
     end
 
     it 'discards any moves which would introduce backtracking' do
-      k = Knight.new([3, 4])
-      k.visited_squares = [[3, 4], [2, 2]]
-      expect(k.possible_next_moves).to eq [[4, 2], [5, 3], [5, 5], [4, 6], [2, 6], [1, 5], [1, 3]]
+      k = Knight.new(starting_position: [3, 4])
+      node1 = Node.new(coordinates: [3, 4])
+      node2 = Node.new(coordinates: [4, 2])
+      node3 = Node.new(coordinates: [5, 3])
+      visited_squares = [node1, node2, node3]
+      expect(k.possible_next_moves(visited_squares)).to eq [[2, 2], [5, 5], [4, 6], [2, 6], [1, 5], [1, 3]]
     end
   end
 end
