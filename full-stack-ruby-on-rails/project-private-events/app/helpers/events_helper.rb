7b57cfc26afb1d88d06e170fef0ae094d2ed6144
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:24:07 -0500

    Allow Users to Subscribe/Unsubscribe From Events in UI

--- full-stack-ruby-on-rails/project-private-events/app/helpers/events_helper.rb
+++ full-stack-ruby-on-rails/project-private-events/app/helpers/events_helper.rb
@@ -1,2 +1,23 @@
 module EventsHelper
+  def friendly_date(event)
+    if event.event_date.year != this_year
+      event.event_date.in_time_zone(system_timezone).strftime('%B %d, %Y')
+    else
+      event.event_date.in_time_zone(system_timezone).strftime('%B %d')
+    end
+  end
+
+  def friendly_time(event)
+    event.event_date.in_time_zone(system_timezone).strftime('%l:%M %p')
+  end
+
+  private
+
+  def system_timezone
+    'Eastern Time (US & Canada)'
+  end
+
+  def this_year
+    Date.today.year
+  end
 end
--- full-stack-ruby-on-rails/project-private-events/app/helpers/users_helper.rb
+++ full-stack-ruby-on-rails/project-private-events/app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def user_attending?(event)
+    current_user.created_events.include?(event) || current_user.attended_events.include?(event)
+  end
 end
