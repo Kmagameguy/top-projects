45380ad0507f792327906f67a79c2bfbc3e8b17f
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:19:56 -0500

    Add Driver Script

---
+++ full-stack-ruby-on-rails/project-connect-four/main.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative './lib/connect_four'
+
+def setup_game
+  puts 'Welcome to Connect Four!'
+  puts 'To get started, please enter your name:'
+
+  c = ConnectFour.new(player_name: player_name)
+  c.play
+end
+
+def player_name
+  loop do
+    name = user_input
+    return name if valid?(name)
+
+    puts "Name can't be empty.  Try again."
+  end
+end
+
+def valid?(name)
+  !name.strip.empty?
+end
+
+def user_input
+  gets.chomp
+end
+
+setup_game
