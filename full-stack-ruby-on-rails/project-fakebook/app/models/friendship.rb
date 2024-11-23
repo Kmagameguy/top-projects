f465aef14d6b0980b3437a13b50e7b2b86b1cefa
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:25:55 -0500

    Validate Friend Requests are Not for Self

--- full-stack-ruby-on-rails/project-fakebook/app/models/friendship.rb
+++ full-stack-ruby-on-rails/project-fakebook/app/models/friendship.rb
@@ -1,7 +1,14 @@
 class Friendship < ApplicationRecord
   validates :user, presence: true
   validates :friend, presence: true, uniqueness: { scope: :user }
+  validate :not_self
 
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  private
+
+  def not_self
+    errors.add(user.name, "can't be friends with themselves!") if user == friend
+  end
 end
