1200de4ccc97f93a6a35b87c11a4cc9e857ac271
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:24:02 -0500

    Allow Users to Create New Events from UI

--- full-stack-ruby-on-rails/project-private-events/app/controllers/events_controller.rb
+++ full-stack-ruby-on-rails/project-private-events/app/controllers/events_controller.rb
@@ -1,5 +1,26 @@
 class EventsController < ApplicationController
+  def new
+    @event = Event.new
+    @time_now = Time.new.strftime('%Y-%m-%dT%k:%M')
+  end
+
   def index
     @events = Event.all
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :location, :event_date)
   end
 end
