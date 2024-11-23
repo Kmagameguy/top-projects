3da4ecb4d9641bf4afb2171086a7f333511daf41
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:25:44 -0500

    Migration to Create Friend Requests Table

--- full-stack-ruby-on-rails/project-fakebook/db/schema.rb
+++ full-stack-ruby-on-rails/project-fakebook/db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_03_182801) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_151739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friend_requests_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_friend_requests_on_user_id_and_friend_id", unique: true
+    t.index ["user_id"], name: "index_friend_requests_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
