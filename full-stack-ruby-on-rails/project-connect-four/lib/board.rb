0b18d4461005e58d597ad07ad7b4538ce22ecb6c
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:19:24 -0500

    Scaffold Connect Four Game

---
+++ full-stack-ruby-on-rails/project-connect-four/lib/board.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# A class to manage our Connect Four game
+class Board; end
---
+++ full-stack-ruby-on-rails/project-connect-four/spec/board_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'board'
+
+RSpec.describe Board do
+end
