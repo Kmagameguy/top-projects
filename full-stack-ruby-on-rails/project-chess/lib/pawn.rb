65604902d2049476314c10d130053e3f417c7dd3
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:20:31 -0500

    Reference ivars Via Getter/Setter Methods

--- full-stack-ruby-on-rails/project-chess/lib/pawn.rb
+++ full-stack-ruby-on-rails/project-chess/lib/pawn.rb
@@ -7,13 +7,13 @@ class Pawn < Piece
   MOVE_SET = [[2, 0], [1, 0]].freeze
 
   def possible_moves
-    x, y = @position
+    x, y = position
     move_set.map { |offset_x, offset_y| [x + offset_x, y + offset_y] }
   end
 
   def move_set
     moves = moved? ? [MOVE_SET.last] : MOVE_SET
-    if @color == :white
+    if color == :white
       moves.map { |x, y| [x * -1, y * -1] }
     else
       moves
@@ -21,6 +21,6 @@ class Pawn < Piece
   end
 
   def promote?
-    (@position[0].zero? || @position[0] == 7)
+    (position[0].zero? || position[0] == 7)
   end
 end
