31899bc0c2e81fa244375b53d4675e4f759d7340
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:26:11 -0500

    Create Comment Model and Wire Up Associations

--- full-stack-ruby-on-rails/project-fakebook/app/models/comment.rb
+++ full-stack-ruby-on-rails/project-fakebook/app/models/comment.rb
@@ -1,10 +1,8 @@
-class Post < ApplicationRecord
+class Comment < ApplicationRecord
   validates :body, presence: true
   validates :user_id, presence: true
+  validates :commentable_id, presence: true
 
+  belongs_to :commentable, polymorphic: true
   belongs_to :author, class_name: "User", foreign_key: :user_id
-
-  has_many :comments, as: :commentable, dependent: :destroy
-
-  default_scope { order(created_at: :desc) }
 end
