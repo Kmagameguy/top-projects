22e86fe3eb75eee5f93df3398572c294e8473a3c
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:16:44 -0500

    Add Node Class This will let us track our Knight's movement history by associating a square with it's parent square (aka the square we moved from before landing on the current square

---
+++ full-stack-ruby-on-rails/project-knights-travails/lib/node.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# A class to manage knight movement over a board
+class Node
+  attr_accessor :coordinates, :parent
+
+  def initialize(coordinates, parent = nil)
+    @coordinates = coordinates
+    @parent = parent
+  end
+end
