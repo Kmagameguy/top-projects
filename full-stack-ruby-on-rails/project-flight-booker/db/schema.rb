6e51b81a07eb301873b89706c29d0c3d90f55e14
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:24:52 -0500

    Create Bookings & Passengers Tables

--- full-stack-ruby-on-rails/project-flight-booker/db/schema.rb
+++ full-stack-ruby-on-rails/project-flight-booker/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_121415) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_134330) do
   create_table "airports", id: false, force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -26,4 +33,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_121415) do
     t.integer "num_tickets", default: 1, null: false
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
+  end
+
+  add_foreign_key "bookings", "flights"
+  add_foreign_key "passengers", "bookings"
 end
