ad7e59c9e45609066e32660d190ac5f36ce3925e
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:26:42 -0500

    Simple Gravatar Integration

--- full-stack-ruby-on-rails/project-fakebook/app/helpers/users_helper.rb
+++ full-stack-ruby-on-rails/project-fakebook/app/helpers/users_helper.rb
@@ -2,4 +2,14 @@ module UsersHelper
   def pending_friend_request?(user)
     FriendRequest.where("friend_id = ? AND user_id = ?", current_user.id, user.id).any?
   end
+
+  def gravatar_for(user)
+    gravatar_url =
+      Rails.cache.fetch("gravatar/#{user.id}", expires_in: 1.hour) do
+        email = user.profile.email || ''
+        gravatar_id = Digest::MD5.hexdigest(email)
+        "https://secure.gravatar.com/avatar/#{gravatar_id}"
+      end
+    image_tag(gravatar_url, alt: user.name)
+  end
 end
