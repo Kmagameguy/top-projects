8cfdea165fb1ae3b28e61199e55eed5acb87d1f8
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:25:39 -0500

    Seed Sample Users

--- full-stack-ruby-on-rails/project-fakebook/db/seeds.rb
+++ full-stack-ruby-on-rails/project-fakebook/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+User.create!(name: 'Roy Trenneman', email: 'roy.trenneman@reynholm.test', password: 'hunter2')
+User.create!(name: 'Maurice Moss', email: 'maurice.moss@reynholm.test', password: '0118999881999197253')
+User.create!(name: 'Jen Barber', email: 'jen.barber@reynholm.test', password: 'theshoes!!!')
+User.create!(name: 'Douglas Reynholm', email: 'douglas@reynholm.test', password: 'elnumero1')
+User.create!(name: 'Richmond Avenal', email: 'richmond@reynholm.test', password: 'goth2boss')
+
+puts "Created #{User.count} users."
