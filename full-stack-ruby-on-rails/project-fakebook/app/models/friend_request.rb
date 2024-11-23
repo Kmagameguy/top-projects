1d099b12be7a2651d2cfa42e84ad1bc228d8f360
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:25:47 -0500

    Disallow Sending Friend Requests to Self

--- full-stack-ruby-on-rails/project-fakebook/app/models/friend_request.rb
+++ full-stack-ruby-on-rails/project-fakebook/app/models/friend_request.rb
@@ -4,4 +4,11 @@ class FriendRequest < ApplicationRecord
 
   validates :user, presence: true
   validates :friend, presence: true, uniqueness: { scope: :user }
+  validate  :not_self
+
+  private
+
+  def not_self
+    errors.add(:friend, "Can't be friends with themselves!") if user == friend
+  end
 end
