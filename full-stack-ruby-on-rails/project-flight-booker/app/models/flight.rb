cefba999bff77922446712cf47c987b4daff0d18
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:24:56 -0500

    Add Model Validations

--- full-stack-ruby-on-rails/project-flight-booker/app/models/flight.rb
+++ full-stack-ruby-on-rails/project-flight-booker/app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
+  validates :departure_airport_id, presence: true
+  validates :arrival_airport_id, presence: true
+  validates :departure_time, presence: true
+
   belongs_to :departure_airport, class_name: "Airport", primary_key: "code"
   belongs_to :arrival_airport, class_name: "Airport", primary_key: "code"
   has_many :bookings
