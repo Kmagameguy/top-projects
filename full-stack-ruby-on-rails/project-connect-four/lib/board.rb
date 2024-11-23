de39b95f77b8cda344b1f03c35936bf3ade0487a
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-11-22 19:19:28 -0500

    Add 'Gravity' to Board

--- full-stack-ruby-on-rails/project-connect-four/lib/board.rb
+++ full-stack-ruby-on-rails/project-connect-four/lib/board.rb
@@ -2,16 +2,27 @@
 
 # A class to manage our Connect Four game
 class Board
+  attr_reader :row_count, :column_count
   attr_accessor :slots
 
   def initialize
-    @rows = 6
-    @columns = 7
-    @slots = Array.new(@rows) { Array.new(@columns) }
+    @row_count = 6
+    @column_count = 7
+    @slots = Array.new(@row_count) { Array.new(@column_count) }
   end
 
   def size
-    { rows: @rows, columns: @columns }
+    { rows: @row_count, columns: @column_count }
+  end
+
+  def drop_to_slot(column, marker)
+    column_index = column - 1
+    @slots.reverse_each do |row|
+      if row[column_index].nil?
+        row[column_index] = marker
+        break
+      end
+    end
   end
 
   def find_slot(row, column)
--- full-stack-ruby-on-rails/project-connect-four/spec/board_spec.rb
+++ full-stack-ruby-on-rails/project-connect-four/spec/board_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Board do
   subject(:board) { Board.new }
   describe '#size' do
     it 'should have a grid size of 7x6 squares' do
-      expect(board.size).to eq({ rows: 6, columns: 7})
+      expect(board.size).to eq({ rows: 6, columns: 7 })
     end
   end
 
@@ -14,6 +14,43 @@ RSpec.describe Board do
     it 'returns the value of the slot at the given coordinates' do
       board.slots[2][5] = 'x'
       expect(board.find_slot(3, 6)).to eql 'x'
+    end
+  end
+
+  describe '#drop_to_slot' do
+    context 'when a chip is added to a column' do
+      it 'puts the chip into the bottom-most slot' do
+        chosen_column = 3
+        bottom_row = board.row_count
+        player_marker = 'x'
+        board.drop_to_slot(chosen_column, player_marker)
+        expect(board.find_slot(bottom_row, chosen_column)).to eql player_marker
+      end
+
+      it 'keeps putting chips into the next-bottom-most slot' do
+        chosen_column = 3
+        player_marker = 'x'
+        bottom_row = board.row_count
+
+        3.times { board.drop_to_slot(chosen_column, player_marker) }
+
+        3.times do |index|
+          expect(board.find_slot(bottom_row - index, chosen_column)).to eql player_marker
+        end
+
+        expect(board.find_slot(bottom_row - 4, chosen_column)).to be_nil
+      end
+
+      it "doesn't allow a player to overwrite an existing chip" do
+        chosen_column = 3
+        player_marker = 'x'
+        computer_marker = 'o'
+        bottom_row = board.row_count
+
+        board.drop_to_slot(chosen_column, player_marker)
+        board.drop_to_slot(chosen_column, computer_marker)
+        expect(board.find_slot(bottom_row, chosen_column)).to eql player_marker
+      end
     end
   end
 end
